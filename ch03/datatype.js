11d5aa53ec301662a7d936030f024077e1b90bd4
Hare <jurun@kakao.com>
2026-03-19 15:50:31 +0900

    2026.03.19 (1)

---
+++ ch03/datatype.js
@@ -0,0 +1,25 @@
+let num = 10; //Number
+num = -1;
+num = 3.14;
+
+let str = "문자열"; //String
+let str2 = '문자열 + num'; //String
+let str3 = `문자열 ${num}`; //String
+
+let bool1 = true; //Boolean
+let bool2 = false; //Boolean
+
+let n = null; // Null
+let u = undefined; // Undefined
+
+let a = {
+    key: "value",
+    key2: "value2",
+} // Object
+
+// a = {}; // Error
+a.key = "modified value";
+
+let arr = [1, 2, 3, 4]; // Array
+arr[0] = 3;
+// arr = [] // Error
---
+++ ch04/assign.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = 3;
+
+console.log((a += b)); // a = a + b, 13
+console.log((a -= b)); // a = a - b, 10
+console.log((a *= b)); // a = a * b, 30
+console.log((a /= b)); // a = a / b, 10
+console.log((a %= b)); // a  = a % b, 1
+console.log((a **= b)); // a = a ** b, 1
+
+a = 1
+
+// 증가 연산자 (전위/후위)
+// ++, --가 앞에 붙으나 뒤에 붙으나
+// a = a + 1 or a = a - 1 연산은 동일하다.
+// 앞에 붙는 경우 전위 연산으로 우선 수행
+// 뒤에 붙는 경우 후위 연산으로 우선순위가 낮다.
+console.log(a++); // 1
+// console.log(a);
+// a = a + 1라 2가 됨
+console.log(++a) // 3
+// a = a + 1라 3이 됨
+// console.log(a); 그대로 3 출력
+console.log(a--); // 3
+console.log(--a) // 1
